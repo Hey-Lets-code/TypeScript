6f126f1081ec49353d322b3b16abfdfed38b0c25
Leticia <le.augusta.souza@gmail.com>
2024-08-19 01:48:41 -0300

    fix: arquivos compilados não estavam criando arquivo js e agora está funcionando.

--- Configurações de ambiente/app/app.ts
+++ Configurações de ambiente/app/app.ts
@@ -1,6 +1,8 @@
 import { Negociacao } from './models/negociacao.js';
+
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
 alert('oi');
+
 // o navegador não entende typescript, ele entende js, por isso iremos criar o tsconfig.js (em dist tem tudo que o navegador)
 // compilação é uma tradução, ou seja estou traduzindo ts para js, que é compreendido pelo computador
