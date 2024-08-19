86a2678369df953cb8bdf18bc803cb616c6d0077
Leticia <le.augusta.souza@gmail.com>
2024-08-19 06:10:48 -0300

    feat: criando as classes privadas e utilizando o npm run start para rodar o script

--- Configurações de ambiente/app/models/negociacao.ts
+++ Configurações de ambiente/app/models/negociacao.ts
@@ -1,27 +1,29 @@
 export class Negociacao {
-    #data;
-    #quantidade;
-    #valor;
+    private _data;
+    private _quantidade;
+    private _valor;
+
+// No javascript para declarar um atributo variável: #data, #quantidade #valor, e para acessar com o get utilizamos this.#data etc.
 
     constructor(data, quantidade, valor) {
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     get data() {
-        return this.#data;
+        return this._data;
     }
 
     get quantidade() {
-        return this.#quantidade;
+        return this._quantidade;
     }
 
     get valor() {
-        return this.#valor;
+        return this._valor;
     }
 
     get volume() {
-        return this.#quantidade * this.#valor;
+        return this._quantidade * this._valor;
     }
 }
